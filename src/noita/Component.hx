5e7a2bfd2b29490b5fb6f227c53a456d633637d6
takashiski <takashiskibb@gmail.com>
2019-12-30 19:59:05 +0900

    update Component

--- src/noita/Component.hx
+++ src/noita/Component.hx
@@ -1,4 +1,5 @@
 package noita;
+import lua.Table;
 import noita.Types;
 
 typedef ComponentId=Int;
@@ -12,19 +13,20 @@ extern class Component
     public static function GetValueFloat( component_id:ComponentId, variable_name:VariableName ):Float;
     public static function GetValueVector2( component_id:ComponentId, variable_name:VariableName ):Vector2;
     public static function GetVectorSize( component_id:ComponentId, array_member_name:String, type_of_vector:Dynamic ):Int;
-    public static function GetVectorValue( component_id:ComponentId, array_name, type, i ) -> multiple return types|nil;
-    public static function GetVector( component_id:ComponentId, array_name, type ) -> {multiple types}|nil;
-    public static function SetValue( component_id:ComponentId, variable_name:VariableName, value ):Void;
-    public static function SetValueVector2( component_id:ComponentId, variable_name:VariableName, x, y ):Void;
-    public static function SetValueValueRange( component_id:ComponentId, variable_name:VariableName, min, max ):Void;
-    public static function SetValueValueRangeInt( component_id:ComponentId, variable_name:VariableName, min, max ):Void;
-    public static function SetMetaCustom( component_id:ComponentId, variable_name:VariableName, value ):Void;
+    // -> multiple return types|nil;
+    public static function GetVectorValue( component_id:ComponentId, array_name:String, type:Any, i:Int ):Null<Any>;
+    // -> {multiple types}|nil;
+    public static function GetVector( component_id:ComponentId, array_name:String, type:Any ):Null<Table<Dynamic,Any>>;
+    public static function SetValue( component_id:ComponentId, variable_name:VariableName, value:Dynamic ):Void;
+    public static function SetValueVector2( component_id:ComponentId, variable_name:VariableName, x:Int, y:Int ):Void;
+    public static function SetValueValueRange( component_id:ComponentId, variable_name:VariableName, min:Int, max:Int ):Void;
+    public static function SetValueValueRangeInt( component_id:ComponentId, variable_name:VariableName, min:Int, max:Int ):Void;
+    public static function SetMetaCustom( component_id:ComponentId, variable_name:VariableName, value:String ):Void;
     public static function GetMetaCustom( component_id:ComponentId, variable_name:VariableName ):Null<String>;
-    public static function GetMembers( component_id:ComponentId ) -> {string-string} [returns a string-indexed table of string];
-    public static function ObjectGetValue( component_id:ComponentId, object_name, variable_name:VariableName ):Null<String>;
-    public static function ObjectGetValueBool( component_id:ComponentId, object_name, variable_name:VariableName ):Null<String>;
-    public static function ObjectGetValue( component_id:ComponentId, object_name, variable_name:VariableName ):Null<String>;
-    public static function ObjectGetValue( component_id:ComponentId, object_name, variable_name:VariableName ):Null<String>;
-    public static function ObjectSetValue( component_id:ComponentId, object_name, variable_name:VariableName, value ):Void;
-    public static function ObjectGetMembers( component_id:ComponentId, object_name ) -> {string-string}
+    //[returns a string-indexed table of string];
+    public static function GetMembers( component_id:ComponentId ):Table<String,String>; 
+    public static function ObjectGetValue( component_id:ComponentId, object_name:String, variable_name:VariableName ):Null<String>;
+    public static function ObjectGetValueBool( component_id:ComponentId, object_name:String, variable_name:VariableName ):Null<String>;
+    public static function ObjectSetValue( component_id:ComponentId, object_name:String, variable_name:VariableName, value:String ):Void;
+  public static function ObjectGetMembers( component_id:ComponentId, object_name:String ):Table<String,String>;
 }
